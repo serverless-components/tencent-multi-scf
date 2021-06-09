a4bc8fcebef19bff9a0adef22f87eb754408acff
yugasun <yuga_sun@163.com>
2021-06-09 15:47:23 +0800

    chore: update lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -68,5 +68,6 @@ module.exports = {
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
   },
 };
